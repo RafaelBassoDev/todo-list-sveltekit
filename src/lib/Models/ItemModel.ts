0c4e90b6f88865c5f6c436a53ee4e0a6501c20b9
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-25 16:56:47 +0200

    feat (models): add 'id' attribute to ItemModel.

--- src/lib/Models/ItemModel.ts
+++ src/lib/Models/ItemModel.ts
@@ -1,8 +1,10 @@
 export default class ItemModel {
+    id: number;
     title: string;
     timestamp: Date;
 
-    constructor(title: string, timestamp: Date) {
+    constructor(id: number, title: string, timestamp: Date) {
+        this.id = id;
         this.title = title;
         this.timestamp = timestamp;
     }
