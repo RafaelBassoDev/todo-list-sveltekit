f4b9a46107fa7d920eb57d47fe5bf0105cc4467f
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-25 22:43:05 +0200

    feat (ItemModel): add 'isChecked' attribute.

--- src/lib/Models/ItemModel.ts
+++ src/lib/Models/ItemModel.ts
@@ -2,10 +2,12 @@ export default class ItemModel {
     id: number;
     title: string;
     timestamp: Date;
+    isChecked: boolean;
 
-    constructor(id: number, title: string, timestamp: Date) {
+    constructor(id: number, title: string, timestamp: Date, isChecked: boolean = false) {
         this.id = id;
         this.title = title;
         this.timestamp = timestamp;
+        this.isChecked = isChecked;
     }
 }
