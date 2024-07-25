f971489ffcea82f70ac1f30971008144c1c069ae
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-25 22:39:53 +0200

    feat (components): create 'ItemList'.

---
+++ src/lib/components/ItemList.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import Item from '$components/Item.svelte';
+    import ItemModel from '$models/ItemModel';
+
+    export let items: ItemModel[];
+</script>
+
+<div class="container">
+    {#if items != null && items.length != 0}
+        {#each items as item}
+            <Item {item} on:edit on:delete on:check />
+        {/each}
+    {:else}
+        <div class="warning">No items!</div>
+    {/if}
+</div>
+
+<style>
+    .container {
+        border-radius: 1em;
+        padding: 1em;
+        background-color: #1e90ff;
+
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+
+    .warning {
+        display: flex;
+        justify-content: center;
+
+        font-size: 2em;
+        padding: 1em;
+    }
+</style>
