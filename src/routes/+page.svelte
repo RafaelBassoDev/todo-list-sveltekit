2dc8b7c69b013308aeaac76caec6fc546d7da745
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-25 14:28:47 +0200

    feat (page): add item list to root page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Item from '$lib/Item.svelte';
+    import ItemModel from '$lib/Models/ItemModel';
+
+    let titles = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+    let date = new Date();
+
+    let models: ItemModel[] = [
+        new ItemModel(
+            'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+            date
+        ),
+        new ItemModel('Lorem ipsum dolor sit amet.', date),
+        new ItemModel('Lorem ipsum dolor sit amet.', date),
+        new ItemModel('Lorem ipsum dolor sit amet.', date)
+    ];
+
+    function onEdit() {
+        console.log('edit');
+    }
+    function onDelete() {
+        console.log('delete');
+    }
+</script>
+
+<div class="container">
+    {#each models as model}
+        <Item {...model} on:edit={onEdit} on:delete={onDelete} />
+    {/each}
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        border: none;
+    }
+    :global(:root) {
+        font-size: 62.5%;
+        margin: 0;
+        padding: 0;
+        border: none;
+    }
+
+    .container {
+        border-radius: 1em;
+        padding: 1em;
+        background-color: #ff6b81;
+
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+</style>
